a8a08cdc3712c86502ac54aca6255b9db874523d
lfnna1977 <lfnna1977@gmail.com>
2024-05-07 01:08:39 -0400

    Create Lista.java

---
+++ TEMA2/GENERICO/Lista.java
@@ -0,0 +1,46 @@
+public class Lista<T> {
+    private Nodo<T> primero;
+    private Nodo<T> ultimo;
+
+    public Lista() {
+        this.primero = null;
+        this.ultimo = null;
+    }
+
+    public void insertarAlInicio(T dato) {
+        Nodo<T> nuevoNodo = new Nodo<>(dato);
+        if (estaVacia()) {
+            primero = nuevoNodo;
+            ultimo = nuevoNodo;
+        } else {
+            nuevoNodo.siguiente = primero;
+            primero = nuevoNodo;
+        }
+    }
+
+    public void insertarAlFinal(T dato) {
+        Nodo<T> nuevoNodo = new Nodo<>(dato);
+        if (estaVacia()) {
+            primero = nuevoNodo;
+            ultimo = nuevoNodo;
+        } else {
+            ultimo.siguiente = nuevoNodo;
+            ultimo = nuevoNodo;
+        }
+    }
+
+    public void visualizar() {
+        Nodo<T> actual = primero;
+        while (actual != null) {
+            System.out.print(actual.dato + " ");
+            actual = actual.siguiente;
+        }
+        System.out.println();
+    }
+
+    public boolean estaVacia() {
+        return primero == null;
+    }
+
+    // Otros métodos de la lista (eliminar, buscar, etc.) pueden ser implementados aquí
+}
