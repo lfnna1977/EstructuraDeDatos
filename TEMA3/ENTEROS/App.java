0f6f99427b0c98836367f5f2d2183b0ec5831518
lfnna1977 <lfnna1977@gmail.com>
2024-05-16 02:07:29 -0400

    Update App.java

--- TEMA3/ENTEROS/App.java
+++ TEMA3/ENTEROS/App.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Árbol Binario de Números Enteros");
@@ -8,7 +6,6 @@ public class App {
         ArbolBinario arbol = new ArbolBinario();
 
         // Insertamos algunos valores en el árbol
-        /*
         arbol.insertar(50);
         arbol.insertar(30);
         arbol.insertar(70);
@@ -16,16 +13,7 @@ public class App {
         arbol.insertar(40);
         arbol.insertar(60);
         arbol.insertar(80);
-        */
-
-        arbol.insertarIterativo(50);
-        arbol.insertarIterativo(30);
-        arbol.insertarIterativo(70);
-        arbol.insertarIterativo(20);
-        arbol.insertarIterativo(40);
-        arbol.insertarIterativo(60);
-        arbol.insertarIterativo(80);
-
+        
         // Realizamos un recorrido preorden para imprimir los nodos del árbol
         System.out.println("Recorrido preorden:");
         arbol.preorden();
@@ -38,14 +26,9 @@ public class App {
         System.out.println("Tamaño del árbol: " + arbol.tamanyo());
 
         // Imprimir el árbol
-        arbol.imprimirArbol();
-
-        // Realizamos un recorrido preorden para imprimir los nodos del árbol
-        System.out.println("Recorrido preorden iterativo:");
-        arbol.recorrerPreordenIterativo();
-        System.out.println();
+        arbol.imprimirArbol();        
 
         // Realiza el recorrido por niveles recursivo del árbol
-        arbol.imprimirPorNivelesConMensaje();
+        arbol.recorridoPorNiveles();
     }    
 }
