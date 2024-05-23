7b293076b321e27a9cffe5e96407447c3c839ad7
lfnna1977 <lfnna1977@gmail.com>
2024-05-23 01:13:17 -0400

    Create App.java

---
+++ EXAMEN/App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Examen Final estructura de datos");
+
+        /*
+         * Nombre Completo: 
+         * 
+         * Problema Nº1
+         * Implementar una función que dado una lista de enteros L y un numero entero n
+         * de forma que con esos datos se modifique la lista mediante el borrado de todos
+         * los elementos de la lista que tengan este valor.
+         * 
+         * Tips
+         * ====
+         * Sacarse una copia del primero nodo
+         * Mientras el nodo sea diferente de null
+         * preguntarnos si el dato del nodo es igual el numero proporcionado
+         * si es positivo actualizar nodos
+         * si no es ir al siguiente nodo
+         * 
+         * Lista de enteros: 2, 4, 5, 6, 2, 8, 5, 2, 10.
+         * funcion(lista de enteros, 2)
+         * Lista de enteros: 4, 5, 6, 8, 5, 10
+         * 
+         * Problema Nº2
+         * Construir una función que sume los elementos de una lista de enteros recursivamente.
+         * 
+         * Tips
+         * ====
+         * Caso Base
+         * Cuando el nodo es null devuelve 0
+         * Caso Recursivo
+         * Del nodo sacar el numero entero y sumarla a la funcion recursiva,
+         * la funcion recursiva se vuelve a llamar pasando como referencia el nodo siguiente.        * 
+         * 
+         * Lista de enteros: 5, 15, 20, 11
+         * La suma es: 51
+         * 
+         * Para ambos problemas, se puede utilizar o listas simplemente enlazadas o doblemente enlazadas.
+         * Utilizar las clases ListaEnlazada.java y ListaDoblementeEnlazada.java proporcionados.
+         */
+    }
+}
